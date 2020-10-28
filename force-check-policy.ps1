615fbba0b4f3775f7655a19376c9d308bf980788
Jordy <31309880+jbarrancos@users.noreply.github.com>
2020-10-28 16:12:07 +0100

    Create force-check-policy.ps1

---
+++ force-check-policy.ps1
@@ -0,0 +1,14 @@
+# Force of policy refresh cycle
+
+Write-Output "Initiate policy refresh cycle for current Subscription"
+$subscriptionId = Get-AzSubscription | Select-Object -ExpandProperty id
+$uri = "https://management.azure.com/subscriptions/$subscriptionId/providers/Microsoft.PolicyInsights/policyStates/latest/triggerEvaluation?api-version=2018-07-01-preview"
+$azContext = Get-AzContext
+$azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+$token = $profileClient.AcquireAccessToken($azContext.Tenant.Id)
+$authHeader = @{
+    'Content-Type'='application/json'
+    'Authorization'='Bearer ' + $token.AccessToken
+}
+Invoke-RestMethod -Method Post -Uri $uri -UseBasicParsing -Headers $authHeader
